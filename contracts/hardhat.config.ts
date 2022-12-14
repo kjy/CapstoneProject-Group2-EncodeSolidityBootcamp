974b92f07588369abaf1ebecbc69a87568937b56
Ryan Strawser <strawser.ryan@gmail.com>
2022-12-14 10:17:07 -0500

    setup hardhat config and dev folders

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -1,8 +1,36 @@
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require('dotenv').config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
+  paths: { tests: "tests" },
+  networks: {
+    hardhat: {
+        chainId: 31337,
+    },
+    goerli: {
+        url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
+        accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        chainId: 5,
+    },
+  },
+  etherscan: {
+    // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+    apiKey: {
+        goerli: `${process.env.ETHERSCAN_API_KEY}`,
+    },
+},
 };
+
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
 
 export default config;
---
+++ contracts/scripts/example.ts
@@ -0,0 +1,19 @@
+
+import * as dotenv from "dotenv";
+dotenv.config()
+
+
+async function main() {
+
+    // script logic goes here
+
+}
+
+
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
+
+// To run file,   yarn run ts-node --files ./scripts/example.ts
