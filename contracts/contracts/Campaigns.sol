437e8d8c150873484f7dc22255f5613eee4f9fab
Ryan Strawser <strawser.ryan@gmail.com>
2022-12-15 12:55:13 -0500

    Update Campaigns.sol
    - Using latest index as campaign ID (TBD)
    - added create campaign
    - added donate
    - added submitPromotion
    
    - added get calls
    
    TODO
    - Add any remaining functionality that may help on frontend after discussing with the team
    
    To disccuss
    - checking for better option instead of using index as campaign ID. The keccak(abi.encode) method.

--- contracts/contracts/Campaigns.sol
+++ contracts/contracts/Campaigns.sol
@@ -2,10 +2,15 @@
 pragma solidity ^0.8.17;
 
 contract Campaigns {
-    // Information about the campaigns
+    // Events
+    event CampaignCreated(uint256 campaignID, address operator, address treasury, string campaignName, string description, uint256 targetGoal, uint256 timestamp);
+    event Donated(uint256 campaignID, address donor, uint256 amount, uint256 timestamp);
+    event PromotionSubmitted(uint256 campaignID, address promoter, uint256 amountPaid, string link, uint256 timestamp);
+
+    // Campaign information
     struct Campaign {
         address operator;
-        address treasury;
+        address payable treasury;
         string campaignName;
         string description;
         uint256 targetGoal;
@@ -15,29 +20,122 @@ contract Campaigns {
     }
 
     // Mapping to keep track of all the campaigns
-    mapping (bytes32 => Campaign) public campaigns;
+    mapping (uint256 => Campaign) public campaigns;
 
-    constructor() {}
+    // Campaign index
+    uint256 public campaignsIndex = 0;
 
-    // Function to create a new campaign
-    function createCampaign(address _operator, address _treasury, string memory _campaignName, string memory _description, uint256 _targetGoal) public {
-        // Generate a unique ID for the campaign
-        bytes32 campaignID = keccak256(abi.encodePacked(_campaignName, _description));
+    // 3 ETH - 3e18
+    uint256 public PROMOTION_PRICE = 3000000000000000000;
 
-        // Create a new campaign using the provided information
-        campaigns[campaignID] = Campaign(_operator, _treasury, _campaignName, _description, _targetGoal, 0, true, block.timestamp);
+    // 1 ETH - 1e18
+    uint256 public MINIMUM_DONATION = 1000000000000000000;
+
+    // The platform treasury address
+    address private platformTreasury;
+
+    constructor(address _platformTreasury) {
+        platformTreasury = _platformTreasury;
     }
 
-    // Function to donate to a campaign
-    function donate(bytes32 _campaignID, uint256 _amount) public payable {
+    // @notice create a new campaign
+    function createCampaign(address _treasury, string memory _campaignName, string memory _description, uint256 _targetGoal) external {
+        require(_targetGoal > 0, "The target goal must be greater than 0.");
+        require(_treasury != address(0), "The treasury address cannot be the zero address.");
+        require(bytes(_campaignName).length > 0, "The campaign name cannot be empty.");
+        require(bytes(_description).length > 0, "The description cannot be empty.");
+
+        // Increment the campaign index
+        uint256 index = campaignsIndex + 1;
+        
+        // Create a new campaign using the provided information
+        campaigns[index] = Campaign(msg.sender, payable(_treasury), _campaignName, _description, _targetGoal, 0, true, block.timestamp);
+
+        // Set the new campaign index
+        campaignsIndex = index;
+
+        // Emit the CampaignCreated event
+        emit CampaignCreated(index, msg.sender, _treasury, _campaignName, _description, _targetGoal, block.timestamp);
+    }
+
+
+    // @notice donate to a campaign
+    function donate(uint256 _campaignID, uint256 _amount) public payable {
+        require(campaigns[_campaignID].operator != address(0), "The campaign does not exist.");
+        require(_amount >= MINIMUM_DONATION, "The donation amount must be greater than or equal to the minimum donation amount.");
+        require(_amount == msg.value, "The donation amount must be equal to the value sent.");
+
         // Get the campaign information
         Campaign storage campaign = campaigns[_campaignID];
 
         // Only allow donations if the campaign is active
         require(campaign.isActive, "This campaign is not active.");
 
-        // Add the donated amount to the total amount donated
-        campaign.totalDonated += _amount;
+        // Transfer the amount to the treasury
+        payable(campaign.treasury).transfer(_amount);
 
+        // Add the donated amount to the total amount donated
+        campaign.totalDonated = campaign.totalDonated + _amount;
+
+        // If the total amount donated is greater than or equal to the target goal, deactivate the campaign
+        if (campaign.totalDonated >= campaign.targetGoal) {
+            campaign.isActive = false;
+        }
+
+        // Emit the Donated event
+        emit Donated(_campaignID, msg.sender, _amount, block.timestamp);
+    }
+
+    // @notice allows promoters to submit a promotion social media link for a campaign.
+    // The promoter must pay the PROMOTION_PRICE to submit a promotion link.
+    // The promoter must submit a link to a social media post that promotes the campaign.
+    // The payment is refunded if the campaign is not active.
+    // The payment is split between the campaign treasury and the platform.
+    function submitPromotion(uint256 _campaignID, string memory _link) public payable {
+        require(campaigns[_campaignID].operator != address(0), "The campaign does not exist.");
+        require(msg.value == PROMOTION_PRICE, "The promotion price must be equal to the value sent.");
+        require(bytes(_link).length > 0, "The link cannot be empty.");
+
+        // Get the campaign information
+        Campaign storage campaign = campaigns[_campaignID];
+
+        // Only allow promotions if the campaign is active
+        require(campaign.isActive, "This campaign is not active.");
+
+        // Calculate the platform fee - 20%
+        uint256 platformFee = PROMOTION_PRICE / 100 * 20;
+
+        // Calculate the campaign fee - 80%
+        uint256 campaignFee = PROMOTION_PRICE / 100 * 80;
+
+        // Transfer the platform and campaign fees to their respective treasuries
+        payable(platformTreasury).transfer(platformFee);
+        payable(campaign.treasury).transfer(campaignFee);
+
+        // Add the donated amount to the total amount donated
+        campaign.totalDonated = campaign.totalDonated + campaignFee;
+
+        // If the total amount donated is greater than or equal to the target goal, deactivate the campaign
+        if (campaign.totalDonated >= campaign.targetGoal) {
+            campaign.isActive = false;
+        }
+
+        // Emit the PromotionSubmitted event
+        emit PromotionSubmitted(_campaignID, msg.sender, PROMOTION_PRICE, _link, block.timestamp);
+    }
+
+    // @notice gets the campaign information
+    function getCampaign(uint256 _campaignID) public view returns (Campaign memory) {
+        return campaigns[_campaignID];
+    }
+
+    // @notice gets the campaign operator
+    function getCampaignOperator(uint256 _campaignID) public view returns (address) {
+        return campaigns[_campaignID].operator;
+    }
+
+    // @notice gets the campaign current index
+    function getLatestCampaignsIndex() public view returns (uint256) {
+        return campaignsIndex;
     }
 }
